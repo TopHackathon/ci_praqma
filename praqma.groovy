0ce9d67fd8c8565f18e3b22275845f9afc3380da
Jon Brohauge <jnz.topdanmark@gmail.com>
2015-06-18 09:59:09 +0200

    fix typo

--- praqma.groovy
+++ praqma.groovy
@@ -5,7 +5,6 @@ import jenkins.model.*;
 Thread.start {
       sleep 10000
       println "--> adding praqma template job"
-      Jenkins.instance.setSlaveAgentPort(50000)
 
       def jobName1 = "praqma-job"
       def configXml1 = new File('/var/jenkins_home/templates/praqma_config.xml').getText('UTF-8')
